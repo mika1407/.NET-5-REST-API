66887c24ec58a53d6596409d9aec040804b88d06
mika tiihonen <koneautomaatio@hotmail.com>
2022-02-23 09:38:46 +0200

    GET toiminto

--- Entities/Repositories/InMemItemsRepository.cs
+++ Entities/Repositories/InMemItemsRepository.cs
@@ -1,20 +1,20 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Catalog.Entities;
 
 namespace Catalog.Repositories
 {
-
-  public class InMemItemsRepository
+  public class InMemItemsRepository : IItemsRepository
   {
     private readonly List<Item> items = new()
     {
-        new Item { Id = Guid.NewGuid(), Name = "Potion", Price = 9, CreatedDate = DateTimeOffset.UtcNow },
-        new Item { Id = Guid.NewGuid(), Name = "Iron Sword", Price = 20, CreatedDate = DateTimeOffset.UtcNow },
-        new Item { Id = Guid.NewGuid(), Name = "Bronze Shield", Price = 18, CreatedDate = DateTimeOffset.UtcNow }
+      new Item { Id = Guid.NewGuid(), Name = "Potion", Price = 9, CreatedDate = DateTimeOffset.UtcNow },
+      new Item { Id = Guid.NewGuid(), Name = "Iron Sword", Price = 20, CreatedDate = DateTimeOffset.UtcNow },
+      new Item { Id = Guid.NewGuid(), Name = "Bronze Shield", Price = 18, CreatedDate = DateTimeOffset.UtcNow }
     };
 
-    public IEnumerable<Item>GetItems()
+    public IEnumerable<Item> GetItems()
     {
       return items;
     }
